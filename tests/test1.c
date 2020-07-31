7a0985c15fe4f8894685a0eaabcd28d87df7087d
Rylan <rylan@riverloopsecurity.com>
2020-07-31 11:57:36 -0400

    add basic test cases

---
+++ tests/test1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void function1(int num) {
+  for(int i = 0; i < num; ++i) {
+    if(i % 2 == 0) {
+      printf("%d", i);
+    }
+    printf("hello world!\n");
+  }
+}
+
+void function2()  {
+  int i = 0;
+  while(i < 50) {
+    ++i;
+    if (i > 40) {
+      printf("%p", &i);
+    }
+  }
+}
+
+int main() {
+  function1(10);
+  function2();
+}
